3a342a08ad629f2177bb88cc8d214c67bda03a3c
emalock3 <emalock3@gmail.com>
2014-02-26 10:22:09 +0900

    Remove unnecessary variables and organize imports

--- src/test/java/com/github/emalock3/camel/issues/ApacheJiraIssuesComponentTest.java
+++ src/test/java/com/github/emalock3/camel/issues/ApacheJiraIssuesComponentTest.java
@@ -1,20 +1,10 @@
 package com.github.emalock3.camel.issues;
 
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 public class ApacheJiraIssuesComponentTest extends CamelTestSupport {
-	
-	@EndpointInject(uri = "mock:result")
-	protected MockEndpoint resultEndpoint;
-	
-	@Produce(uri = "direct:start")
-	protected ProducerTemplate template;
 
 	@Test
 	public void test() {
